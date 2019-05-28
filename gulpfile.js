ab11a1e1a3d13c34b16be0cad1dbb93807e36469
Vyacheslav Vasilenko <vasilenko.vyacheslav@catenamedia.com>
2019-05-28 09:16:47 +0300

    update proj

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 'use strict';
 var gulp = require('gulp'),
+    watch = require('gulp-watch'),
     uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
     sass = require('gulp-sass');
 
 
@@ -9,7 +14,10 @@ var gulp = require('gulp'),
 gulp.task('sass', async function() {
     gulp.src('./app/sass/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths: require('node-normalize-scss').includePaths
+        }))
         .pipe(autoprefixer(['last 2 version']))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css'))
@@ -18,14 +26,23 @@ gulp.task('sass', async function() {
 gulp.task('minify', async function () {
     gulp.src('app/js/*.js')
         .pipe(uglify())
+        .pipe(concat('main.js', {newLine: ';'}))
         .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('watch', [ 'build' ], function() {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch(('js/*.js'), ['minify']);
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
 });
 
-gulp.task('build', ['sass',  'minify']);
+
+gulp.task('watch', [ 'build' ], function() {
+    gulp.watch('app/sass/**/*.scss', ['sass']);
+    gulp.watch(('app/js/*.js'), ['minify']);
+    gulp.watch('app/images/**/*.*', ['images']);
+});
+
+gulp.task('build', ['sass',  'minify', 'images']);
 
 gulp.task('default', ['watch']);
